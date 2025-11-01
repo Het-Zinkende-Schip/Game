b9f3459552af987bbac9e8c8c0ac23a92f98c953
Nicolas Dickinson <dickinson@washnote.com>
2025-11-01 03:50:21 +0100

    activate place markers and activate modal

--- js/gui-examples.js
+++ js/gui-examples.js
@@ -15,9 +15,67 @@
     userChoice = true,
     canClose = false,
     callback = function(value) {
-        
+
     }
   )
 
 zoomToPlace(lng = 10, lat = 10)
 
+hideAllInstruments()
+showInstrument(4)
+
+  const examplePlaceOptions = [
+    {
+      location: [0,0],
+      value: "place1"
+    },
+        {
+      location: [10,10],
+      value: "place2"
+    },
+    {
+      location: [20,20],
+      value: "place3"
+    }
+  ]
+
+
+  let activeMarkers = [];
+
+  function activatePlaceChoiceMarkers(placeOptions = [], callback = v => console.log('User chose:', v)) {
+    // remove any existing markers first
+    activeMarkers.forEach(m => m.remove());
+    activeMarkers = [];
+
+    placeOptions.forEach(opt => {
+      const el = document.createElement('img');
+      el.src = 'icons/anker-icon.png';
+      el.style.width = '4em';
+      el.className = 'place-marker';
+ 
+      el.addEventListener('click', () => {
+        callback(opt.value);
+        // remove all markers after a selection
+        activeMarkers.forEach(m => m.remove());
+        activeMarkers = [];
+      });
+
+      const marker = new maplibregl.Marker({ element: el })
+        .setLngLat(opt.location)
+        .addTo(map);      
+      activeMarkers.push(marker);
+    }
+    
+    );
+
+
+    for(i=0;i<placeOptions.length;i++) {
+      new maplibregl.Marker({element: el})
+      .setLngLat([4.9, 52.37])
+      .addTo(map);
+    }
+  }
+
+  hideAllInstruments()
+  showInstrument(4)
+  activatePlaceChoiceMarkers(examplePlaceOptions)
