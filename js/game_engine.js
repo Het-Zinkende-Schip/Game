975cd4b6a0910dc5522dae9c39fd459301bafc6e
Bob Coret <bob.coret+github@gmail.com>
2025-11-01 09:27:51 +0100

    updates

--- js/game_engine.js
+++ js/game_engine.js
@@ -13,36 +13,32 @@ var bemanning;
 var haven_uri;
 var guessedPlaceUri;
 
-gui_init();	// resets spelbord (kaart tekening, dobbelstenen, brandende kaars, puntpasser + 4 instrumentplekken)
+console.log("Game engine loaded");
+//gui_init();	// resets spelbord (kaart tekening, dobbelstenen, brandende kaars, puntpasser + 4 instrumentplekken)
 
 toon_startscherm();
 
-let kaars_id="kaars";				// identifier van de div in gui
-let puntpasser_id="puntpasser";		// identifier van de div in gui
-let dobbelsteen_id="dobbelsteen";	// identifier van de div in gui
-	
 document.getElementById(kaars_id).addEventListener("click", function() {
-  toon_kaars();
+//	document.getElementById("puntpasser").addEventListener("click", function() {
+//		document.getElementById("dobbelsteen").addEventListener("click", function() {
+			game_play();
+//		});
+//		toon_puntpasser();
+//	});
+//	toon_kaars();
 });
 
-document.getElementById(puntpasser_id).addEventListener("click", function() {
-  toon_puntpasser();
-});
-
-document.getElementById(dobbelsteen_id).addEventListener("click", function() {
-  game_play();
-});
-
-function game_play(dialog_uri) {
+async function game_play(dialog_uri) {
 
 	switch (stage) {
 	  case 1:
 		console.log("Stage 1: Kies je vertrekhaven");
 		if (reis==1) {
-			havens=get_lijst_vertrekhavens("NL");
+			havens=await get_lijst_vertrekhavens("NL");
 		} else {
-			havens=get_lijst_vertrekhavens("all");
+			havens=await get_lijst_vertrekhavens("all");
 		}
+		console
 		stage++;
 		toon_keuze_dialoog_vertrekhaven(havens);
 		break;
@@ -181,7 +177,7 @@ function toon_keuze_dialoog_vertrekhaven(havens) {
 	activateModal(
 		title = "Kies je vertrekhaven",
 		text = "Vanuit welke haven wil je vertrekken?",
-		choicesObject = havens.map(haven => ({ 'html': haven.naam, 'value': haven.uri })),
+		choicesObject = havens.slice(0,6).map(haven => ({ 'html': haven.departurePlace, 'value': haven.departurePlaceUri })),
 		mp3 = null,
 		userChoice = true,
 		canClose = false,
@@ -193,8 +189,8 @@ function toon_startscherm() {
 	// open dialog met intro tekst + plaatje + button "start de reis", opening tune?
 	activateModal(
 		title = "Welkom bij het Zinkende Schip!",
-		text = "Welkom bij *Het Zinkende Schip*, cartograaf! In dit point-en-click spel monster je als leerling cartograaf aan bij verschillende VOC reizen. Terwijl je aan boord een historisch persoon schaduwt, en op basis van data uit onder andere Maritime Stepping Stones (MaSS), het Huygens instituut en het Nationaal Archief, verdien jij je cartografie-gereedschappen. Wat leer jij onderweg? En lukt het je op te klimmen van leerling naar gezel?",
-		choicesObject = null,
+		text = "Welkom bij <i>Het Zinkende Schip</i>, cartograaf! In dit point-en-click spel monster je als leerling cartograaf aan bij verschillende VOC reizen. Terwijl je aan boord een historisch persoon schaduwt, en op basis van data uit onder andere Maritime Stepping Stones (MaSS), het Huygens instituut en het Nationaal Archief, verdien jij je cartografie-gereedschappen. Wat leer jij onderweg? En lukt het je op te klimmen van leerling naar gezel?",
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = true,
@@ -207,7 +203,7 @@ function toon_kaars() {
 	activateModal(
 		title = "De schaduwkant van het Zinkende Schip!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = true,
@@ -220,7 +216,7 @@ function toon_puntpasser() {
 	activateModal(
 		title = "Over cartografie",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = true,
@@ -232,11 +228,11 @@ function toon_uitleg_markers(place_name) {
 	activateModal(
 		title = "Weet je waar deze haven van "+place_name+" ligt?",
 		text = "klik zo op een marker op de kaart om de locatie van de haven aan te geven.",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)	
 }
 
@@ -244,23 +240,38 @@ function toon_dialoog_minigame_plaats_goed() {
 	activateModal(
 		title = "Goede plaats gekozen!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)
 }
+
+function show_minigame_plaats(havens) {
+	//	haven_uri is gekozen haven, nog max 5 random havens erbij tonen
+	//  add markers, array van lat,lon 
+
+	const placeOptions = havens
+	.filter(haven => !haven.placeId) // keep only items without placeId
+	.map(haven => ({
+		value: haven.placeId,
+		longitude: haven.longitude,
+		latitude: haven.latitude
+	})).slice(0,5);
+
+	activatePlaceChoiceMarkers(placeOptions = [], game_play());
+};
 
 function toon_dialoog_minigame_plaats_fout() {
 	activateModal(
 		title = "Foute plaats gekozen!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)
 }
 
@@ -270,7 +281,8 @@ function toon_keuze_dialoog_scheepstype(voyages, scheeptypen) {
 	activateModal(
 		title = "Kies je schip",
 		text = "Op welk schip wil je varen?",
-		choicesObject = scheeptypen.map(scheeptype => ({ 'html': scheeptype.naam+'<br>'+scheeptype.iiif_image_uri, 'value': scheeptype.uri })),
+		choicesObject = scheeptypen.map(scheeptype => (
+			{ 'html': scheeptype.naam+'<br><img src="'+scheeptype.iiif_image_uri+'" alt="'+scheeptype.naam+'"/>', 'value': scheeptype.uri })),
 		mp3 = null,
 		userChoice = true,
 		canClose = false,
@@ -294,11 +306,11 @@ function toon_levensloop_reizen(levensloop) {
 	activateModal(
 		title = "Levensloop van bemanningslid {naam}",
 		text = "lijst van reizen TODO....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = true,
-		callback = gameplay()
+		callback = game_play()
 	)	
 }
 
@@ -306,11 +318,11 @@ function toon_dialoog_minigame_scheeptype_goed() {
 	activateModal(
 		title = "Goede scheepstype gekozen!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)
 }
 
@@ -318,11 +330,11 @@ function toon_dialoog_minigame_scheeptype_fout() {
 	activateModal(
 		title = "Foute scheepstype gekozen!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)
 }
 
@@ -330,11 +342,11 @@ function toon_scheepskist(object_title, iiif_image_uri, description) {
 	activateModal(
 		title = "Nieuw item voor in je scheepskist!",
 		text = "intro....",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
-		callback = gameplay()
+		callback = game_play()
 	)	
 }
 
@@ -346,12 +358,12 @@ function toon_extra_cartografische_instrument(reis) {
 function toon_einde_reis() { // hoe lang erover gedaan, aanzetten tot nog een reis
 	activateModal(
 		title = "Einde van de reis!",
-		text = "Je hebt dezze reis succesvol afgerond, het duurde x dagen, op naar de volgende reis voor nog meer ervaring op te doen.",
-		choicesObject = null,
+		text = "Je hebt deze reis succesvol afgerond, het duurde x dagen, op naar de volgende reis voor nog meer ervaring op te doen.",
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = true,
-		callback = gameplay()
+		callback = game_play()
 	)	
 }
 
@@ -359,7 +371,7 @@ function end_of_game() { // dialoog die spel stopt, enige optie is F5 om nieuwe 
 	activateModal(
 		title = "Einde van het spel!",
 		text = "Schip gezonken, einde van het spel. F5 om opnieuw te starten.",
-		choicesObject = null,
+		choicesObject = [],
 		mp3 = null,
 		userChoice = false,
 		canClose = false,
